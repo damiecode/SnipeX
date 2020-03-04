bd97edd1b0e691c6833f3fd411fdc5c927f55370
damiecode <codenlyn@gmail.com>
2020-03-04 22:26:44 +0100

    Correct linter erors

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,49 @@
-const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app: "./src/index.js",
-    "production-dependencies": ["phaser"]
+    app: './src/index.js',
+    'production-dependencies': ['phaser'],
   },
 
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "[name].js"
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js',
   },
   optimization: {
-    minimize: true
+    minimize: true,
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: path.resolve(__dirname, "src/"),
+        include: path.resolve(__dirname, 'src/'),
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
   },
 
   plugins: [
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, "assets", "**", "*"),
-        to: path.resolve(__dirname, "build")
-      }
+        from: path.resolve(__dirname, 'assets', '**', '*'),
+        to: path.resolve(__dirname, 'build'),
+      },
     ]),
     new webpack.DefinePlugin({
-      "typeof CANVAS_RENDERER": JSON.stringify(true),
-      "typeof WEBGL_RENDERER": JSON.stringify(true)
-    })
+      'typeof CANVAS_RENDERER': JSON.stringify(true),
+      'typeof WEBGL_RENDERER': JSON.stringify(true),
+    }),
   ],
 
   devServer: {
-    contentBase: path.resolve(__dirname, "build")
-  }
+    contentBase: path.resolve(__dirname, 'build'),
+  },
 };
